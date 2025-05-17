f0db73e5c922d3a8de05a577b4d7adce26573e4c
João Conserva <boscoj@outlook.com>
2025-05-17 09:18:41 -0300

    feat: add page content type with dynamic zones and related components
    - Created a new collection type for pages with attributes: title, slug, and blocks.
    - Implemented controller, service, and router for the page API.
    - Added shared components: CTA, ImageBlock, TextBlock, and a test component.
    - Defined schemas for shared components in the generated types.

---
+++ types/generated/components.d.ts
@@ -0,0 +1,137 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedBlocks extends Struct.ComponentSchema {
+  collectionName: 'components_shared_blocks';
+  info: {
+    description: '';
+    displayName: 'blocks';
+  };
+  attributes: {
+    CTA: Schema.Attribute.Component<'shared.cta', false>;
+    ImageBlock: Schema.Attribute.Component<'shared.image-block', false>;
+    TextBlock: Schema.Attribute.Component<'shared.text-block', false>;
+  };
+}
+
+export interface SharedCta extends Struct.ComponentSchema {
+  collectionName: 'components_shared_ctas';
+  info: {
+    displayName: 'CTA';
+  };
+  attributes: {
+    label: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface SharedImageBlock extends Struct.ComponentSchema {
+  collectionName: 'components_shared_image_blocks';
+  info: {
+    displayName: 'ImageBlock';
+  };
+  attributes: {
+    media: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+  };
+}
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
+export interface SharedTest extends Struct.ComponentSchema {
+  collectionName: 'components_shared_tests';
+  info: {
+    displayName: 'test';
+  };
+  attributes: {
+    test: Schema.Attribute.String;
+  };
+}
+
+export interface SharedTextBlock extends Struct.ComponentSchema {
+  collectionName: 'components_shared_text_blocks';
+  info: {
+    displayName: 'TextBlock';
+  };
+  attributes: {
+    text: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.blocks': SharedBlocks;
+      'shared.cta': SharedCta;
+      'shared.image-block': SharedImageBlock;
+      'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
+      'shared.test': SharedTest;
+      'shared.text-block': SharedTextBlock;
+    }
+  }
+}
